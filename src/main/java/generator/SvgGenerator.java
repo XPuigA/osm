2855d510182dda46a91e4cc0f00713b37e14728c
Xavi Puig <xpuig89@gmail.com>
2024-02-13 21:29:58 +0100

    First normalization version

--- src/main/java/generator/SvgGenerator.java
+++ src/main/java/generator/SvgGenerator.java
@@ -4,15 +4,12 @@ import model.*;
 
 public class SvgGenerator implements Generator {
 
-    public static int DEFAULT_WIDTH = 100;
-    public static int DEFAULT_HEIGHT = 100;
+    public static int DEFAULT_WIDTH = 400;
+    public static int DEFAULT_HEIGHT = 400;
 
     private OsmMap map;
     private int width;
     private int height;
-
-    private double scaleX;
-    private double scaleY;
 
     public SvgGenerator(OsmMap map) {
         this(map, DEFAULT_WIDTH, DEFAULT_HEIGHT);
@@ -33,23 +30,23 @@ public class SvgGenerator implements Generator {
                 OsmNode node2 = map.getNodes().get(way.getNds().get(i).getRef());
                 sb.append("\t\t<line")
                     .append(" x1=\"").append(scaleLatitude(node1.getLat()))
-                    .append("\" y1=\"").append(scaleLatitude(node1.getLon()))
+                    .append("\" y1=\"").append(scaleLongitude(node1.getLon()))
                     .append("\" x2=\"").append(scaleLatitude(node2.getLat()))
-                    .append("\" y2=\"").append(scaleLatitude(node2.getLon()))
-                    .append("\" style=\"stroke:red;stroke-width:2\" />\n");
+                    .append("\" y2=\"").append(scaleLongitude(node2.getLon()))
+                    .append("\" style=\"stroke:red;stroke-width:1\" />\n");
             }
         }
         sb.append("</svg>");
         return sb.toString();
     }
 
-    // TODO
     private double scaleLatitude(double latitude) {
-        return 0.0;
+        double xNorm = (latitude - map.getBounds().getMinLat()) / (map.getBounds().getMaxLat() - map.getBounds().getMinLat());
+        return xNorm * width;
     }
 
-    // TODO
     private double scaleLongitude(double longitude) {
-        return 0.0;
+        double yNorm = (longitude - map.getBounds().getMinLon()) / (map.getBounds().getMaxLon() - map.getBounds().getMinLon());
+        return yNorm * height;
     }
 }
